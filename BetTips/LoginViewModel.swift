1e84db2335c3c60cf6950fef9c681ac09f7df534
mspoonam <mspoonam.learning@gmail.com>
2023-01-30 06:32:10 -0500

    fix: LoginViewModel error

--- BetTips/LoginViewModel.swift
+++ BetTips/LoginViewModel.swift
@@ -1,66 +1,50 @@
-//
-//  LoginViewModel.swift
-//  Asset Tracker
-
-
 import SwiftUI
 import Combine
 
-extension LoginView {
+class LoginViewModel: ObservableObject {
     
-    class LoginViewModel: ObservableObject {
-        
-        // MARK: Must have
-        
-        
-        // MARK: View state
-        @Published var loginForm = LoginFormState(txtUsername: "", txtPassword: "")
-        
-        
-        @Published var checkBox = false
-        
-        // MARK: Errors
-        @Published var emailValidationFail = false
-        @Published var passwordValidationFail = false
-        @Published var loginFail = (false, "")
-        
-        
-        // MARK: Loader
-        @Published var showLoader = false
-        
-        // MARK: Init
-        
-        init() {}
-        
-        
-        // MARK: User actions
-        
-        // MARK: Validations
-        func formValidate() -> Bool {
-            if loginForm.txtUsername.isnotEmpty {
-                if !loginForm.txtPassword.isEmpty {
-                    return true
-                } else {
-                    passwordValidationFail = true
-                    return false
-                }
+    // MARK: Must have
+    
+    
+    // MARK: View state
+    @Published var loginForm = LoginFormState(txtUsername: "", txtPassword: "")
+    
+    
+    @Published var checkBox = false
+    
+    // MARK: Errors
+    @Published var emailValidationFail = false
+    @Published var passwordValidationFail = false
+    @Published var loginFail = (false, "")
+    
+    
+    // MARK: Loader
+    @Published var showLoader = false
+    
+    // MARK: Init
+    init() {}
+            
+    // MARK: Validations
+    func formValidate() -> Bool {
+        if !loginForm.txtUsername.isEmpty {
+            if !loginForm.txtPassword.isEmpty {
+                return true
             } else {
-                emailValidationFail = true
+                passwordValidationFail = true
                 return false
             }
+        } else {
+            emailValidationFail = true
+            return false
         }
-        
-        // MARK: API
-        func login() {}
-        
-        // MARK: Form
-        extension LoginView {
-            struct LoginFormState {
-                var txtUsername: String = ""
-                var txtPassword: String = ""
-            }
-        }
-        
-        
     }
+    
+    // MARK: API
+    func login() {}
+   
+}
+
+struct LoginFormState {
+    var txtUsername: String = ""
+    var txtPassword: String = ""
 }
